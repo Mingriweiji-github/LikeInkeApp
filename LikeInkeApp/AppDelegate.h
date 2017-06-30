e41b8b80898f48c5d8bc2c41c6e4d13f84982b12
Mingriweiji-github <2822072760@qq.com>
2017-06-30 16:22:18 +0800

    init project

---
+++ LikeInkeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LikeInkeApp
+//
+//  Created by M_Li on 2017/6/30.
+//  Copyright © 2017年 M_Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
